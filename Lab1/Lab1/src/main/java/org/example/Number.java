b0c1ee2d9a017a9183d57542c43d32c198f7a98f
Felix140301 <orgafelixcosmin@gmail.com>
2024-03-10 22:31:58 +0200

    Lab1 Solution

---
+++ Lab1/Lab1/src/main/java/org/example/Number.java
@@ -0,0 +1,22 @@
+package org.example;
+
+public class Number {
+    private int real=0;
+    private int imag=0;
+
+    public void setReal(int real) {
+        this.real = real;
+    }
+
+    public void setImag(int imag){
+        this.imag=imag;
+    }
+
+    public int getImag() {
+        return imag;
+    }
+
+    public int getReal() {
+        return real;
+    }
+}
