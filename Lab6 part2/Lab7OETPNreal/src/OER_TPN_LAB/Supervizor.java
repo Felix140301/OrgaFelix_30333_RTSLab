2f40d7c3b051572af0086134c21737a8ef6b32dc
Felix140301 <orgafelixcosmin@gmail.com>
2024-05-26 23:46:22 +0300

    Added Lab 6 part 2
    (last lab session)

---
+++ Lab6 part2/Lab7OETPNreal/src/OER_TPN_LAB/Supervizor.java
@@ -0,0 +1,136 @@
+
+package OER_TPN_LAB;
+
+import Components.Activation;
+import Components.Condition;
+import Components.GuardMapping;
+import Components.PetriNet;
+import Components.PetriNetWindow;
+import Components.PetriTransition;
+import DataObjects.DataInteger;
+import DataObjects.DataREL;
+import DataObjects.DataRELQueue;
+import DataObjects.DataTransfer;
+import DataOnly.RELQueue;
+import DataOnly.TransferOperation;
+import Enumerations.LogicConnector;
+import Enumerations.TransitionCondition;
+import Enumerations.TransitionOperation;
+
+import java.util.ArrayList;
+
+
+public class Supervizor {
+    public static void main(String[] args) {
+
+        PetriNet pn = new PetriNet();
+        pn.PetriNetName = "Supervisor PN";
+        pn.NetworkPort = 1082;
+
+        DataREL ps_i1 = new DataREL();
+        ps_i1.SetName("ps_i1");
+        pn.PlaceList.add(ps_i1);
+
+        DataRELQueue ps_1 = new DataRELQueue();
+        ps_1.SetName("ps_1");
+        pn.PlaceList.add(ps_1);
+
+
+
+        DataInteger ps_2 = new DataInteger();
+        ps_2.SetName("ps_2");
+        ps_2.SetValue(1);
+        pn.PlaceList.add(ps_2);
+
+        DataInteger ps_3 = new DataInteger();
+        ps_3.SetName("ps_3");
+        ps_3.SetValue(1);
+        pn.PlaceList.add(ps_3);
+
+        DataTransfer ps_o1 = new DataTransfer();
+        ps_o1.SetName("ps_o1");
+        ps_o1.Value = new TransferOperation("localhost", "1080", "p_i1");
+        pn.PlaceList.add(ps_o1);
+
+        DataInteger ps_o2 = new DataInteger();
+        ps_o2.SetName("ps_o2");
+        pn.PlaceList.add(ps_o2);
+
+        DataInteger ps_i2 = new DataInteger();
+        ps_i2.SetName("ps_i2");
+        pn.PlaceList.add(ps_i2);
+
+        //_____________________Ts 1___________________________________
+
+        PetriTransition ts_1 = new PetriTransition(pn);
+        ts_1.TransitionName = "ts_1";
+        ts_1.InputPlaceName.add("ps_i1");
+        ts_1.InputPlaceName.add("ps_1");
+
+
+        Condition TS1Ct1 = new Condition(ts_1, "ps_i1", TransitionCondition.NotNull);
+        Condition TS1Ct2 = new Condition(ts_1, "ps_1", TransitionCondition.NotNull);
+        TS1Ct1.SetNextCondition(LogicConnector.AND, TS1Ct2);
+
+        GuardMapping grdTS1 = new GuardMapping();
+        grdTS1.condition = TS1Ct1;
+
+        grdTS1.Activations.add(new Activation(ts_1, "ps_i1", TransitionOperation.AddElement, "ps_1"));
+        ts_1.GuardMappingList.add(grdTS1);
+        ts_1.Delay = 0;
+        pn.Transitions.add(ts_1);
+
+        //_____________________Ts 2___________________________________
+        PetriTransition ts_2 = new PetriTransition(pn);
+        ts_2.TransitionName = "ts_2";
+        ts_2.InputPlaceName.add("ps_1");
+        ts_2.InputPlaceName.add("ps_2");
+        ts_2.InputPlaceName.add("ps_3");
+
+        Condition TS2Ct1 = new Condition(ts_2, "ps_1", TransitionCondition.HaveREL);
+        Condition TS2Ct2 = new Condition(ts_2, "ps_2", TransitionCondition.NotNull);
+        Condition TS2Ct3 = new Condition(ts_2, "ps_3", TransitionCondition.NotNull);
+
+        TS2Ct2.SetNextCondition(LogicConnector.AND, TS2Ct3);
+        TS2Ct1.SetNextCondition(LogicConnector.AND, TS2Ct2);
+
+
+        GuardMapping grdTS2 = new GuardMapping();
+        grdTS2.condition = TS2Ct1;
+
+        ArrayList<String> ts2Output = new ArrayList<String>();
+        ts2Output.add("ps_o1");
+        ts2Output.add("ps_3");
+        ts2Output.add("ps_1");
+        grdTS2.Activations.add((new Activation(ts_2, "ps_1", TransitionOperation.SendROverNetwork, "ps_o1")));
+        grdTS2.Activations.add(new Activation(ts_2,"ps_1", TransitionOperation.PopElement_R_E,  ts2Output));
+
+
+        ts_2.GuardMappingList.add(grdTS2);
+        ts_2.Delay  = 0;
+        pn.Transitions.add(ts_2);
+
+        //_____________________Ts 3___________________________________
+        PetriTransition ts_3 = new PetriTransition(pn);
+        ts_3.TransitionName = "ts_3";
+        ts_3.InputPlaceName.add("ps_i2");
+
+        Condition TS3Ct1 = new Condition(ts_3, "ps_i2", TransitionCondition.NotNull);
+        GuardMapping grdTS3 = new GuardMapping();
+        grdTS3.condition = TS3Ct1;
+
+        grdTS3.Activations.add(new Activation(ts_3, "ps_i2", TransitionOperation.Copy, "ps_2"));
+        grdTS3.Activations.add(new Activation(ts_3, "ps_i2", TransitionOperation.Move, "ps_o2"));
+
+        ts_3.GuardMappingList.add(grdTS3);
+        ts_3.Delay = 0;
+        pn.Transitions.add(ts_3);
+
+        pn.Delay = 2000;
+        PetriNetWindow frame = new PetriNetWindow(false);
+        frame.petriNet  = pn;
+        frame.setVisible(true);
+
+    }
+
+}
